c46493aca4c62c47566823cf269fa29968f0e0e6
Andreas Stocker <shadowstep7@gmail.com>
2017-09-08 19:47:11 +0200

    angular works! hello hangular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<h1>Test</h1>'
+  template: '<h1>Test</h1><br><input type="button" value="click here" />'
 })
 export class AppComponent {
   title = 'app';
