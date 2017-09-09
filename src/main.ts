b22fc47e22b979ac1579d2f6e8b202cb86d1852a
hb20007 <hzsababa@outlook.com>
2017-09-09 17:41:19 +0300

    Trying to get env vars to work

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,10 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
 
-if (environment.production) {
-  enableProdMode();
-}
+// if (NODE_ENV === 'production') {
+//   enableProdMode();
+// }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,10 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
                 test: /\.tsx?$/,
                 use: [
                     // {
