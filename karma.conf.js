96c1827f233ac91860ab8e5150f0f4ce650b5b2f
Andreas Stocker <shadowstep7@gmail.com>
2017-09-12 17:10:05 +0200

    unit tests - setup karma

---
+++ karma.conf.js
@@ -0,0 +1,26 @@
+const webpackConfig = require('./webpack.config');
+
+// Karma configuration
+module.exports = function (config) {
+    config.set({
+        frameworks: ['jasmine'],
+
+        files: [
+            { pattern: 'src/*.spec.ts', watched: false },
+            { pattern: 'src/**/*.spec.ts', watched: false }
+        ],
+
+        preprocessors: {
+            'src/*.spec.ts': ['webpack', 'sourcemap'],
+            'src/**/*.spec.ts': ['webpack', 'sourcemap'],
+        },
+
+        webpack: { ...webpackConfig, output: undefined, entry: undefined, devtool: 'inline-source-map' },
+
+        webpackMiddleware: {
+            stats: 'errors-only'
+        },
+
+        browsers: ['PhantomJS']
+    });
+};
