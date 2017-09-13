ad0f657cb98d775b6b484c2dd7f683d8d4502d1f
mrbzs <mrbzs123@hotmail.com>
2017-09-13 20:30:37 +0300

    [SPC-nav_bar] First layout

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 import { AppComponent } from './components/app/app';
-import { NavbarComponent } from "./components/nav-bar/nav-bar";
+import { NavbarComponent } from "./components/nav-bar/nav";
 import { LandingpageComponent } from "./components/landing-page/landing";
 import { firebaseConfig } from './firebase.config';
 import { SignUpFormComponent } from './components/sign-up-form/sign-up-form';
--- src/components/app/app.ts
+++ src/components/app/app.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import * as AppTpl from './app.html';
 
 @Component({
-  selector: 'app-root',
-  template: AppTpl
+    selector: 'app-root',
+    template: AppTpl
 })
 export class AppComponent {
-  title = 'app';
+
 }
 
