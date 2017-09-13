96453d61700c59138d012a3705ee2dddcefb8ee5
Andreas Stocker <shadowstep7@gmail.com>
2017-09-13 19:34:42 +0300

    add angular fire, sign up form and login comp

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 import { AppComponent } from './components/app/app';
 import { NavbarComponent } from "./components/nav-bar/nav-bar";
 import { LandingpageComponent } from "./components/landing-page/landing";
+import { firebaseConfig } from './firebase.config';
+import { SignUpFormComponent } from './components/sign-up-form/sign-up-form';
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule,
+  ],
   declarations: [
     AppComponent,
     NavbarComponent,
-    LandingpageComponent
-  ],
-  imports: [
-    BrowserModule
+    LandingpageComponent,
+    SignUpFormComponent,
   ],
   providers: [],
   bootstrap: [AppComponent]
