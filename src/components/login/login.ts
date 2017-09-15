e9ea2f7a28288ac998b618e38430034f26072673
Andreas Stocker <shadowstep7@gmail.com>
2017-09-15 20:58:43 +0300

    [SPC-login] login page fully functional and styled

--- src/components/login/login.ts
+++ src/components/login/login.ts
@@ -4,6 +4,7 @@ import { AngularFireAuth } from 'angularfire2/auth';
 import { Component } from "@angular/core";
 import * as firebase from 'firebase/app';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 // partof: #SPC-login
 @Component({
@@ -13,15 +14,30 @@ import { NgForm } from '@angular/forms';
 })
 export class LoginComponent {
     fields = { email: '', password: '' }
+    submissionError = '';
 
-    constructor(private angularFireAuth: AngularFireAuth) { }
+    constructor(private router: Router, private angularFireAuth: AngularFireAuth) { }
 
     emailLogin() {
+        this.submissionError = '';
+
         this.angularFireAuth.auth.signInWithEmailAndPassword(this.fields.email, this.fields.password)
-            .then((res) => {
-                console.log(res);
+            .then(() => {
                 this.fields.email = '';
                 this.fields.password = '';
+                this.router.navigateByUrl('/levels');
+            }).catch((err: any) => {
+                switch (err.code) {
+                    case 'auth/invalid-email':
+                        this.submissionError = 'Invalid Email Address';
+                        break;
+                    case 'auth/wrong-password':
+                        this.submissionError = 'Wrong Password';
+                        break;
+                    default:
+                        this.submissionError = 'Unknown Error';
+                }
+                throw err;
             });
     }
 }
