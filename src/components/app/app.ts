67f100b7d7dc0c3def4901094a4d31b87e2c315f
Andreas Stocker <shadowstep7@gmail.com>
2017-09-15 18:14:58 +0300

    [SPC-login] add styles to login form

--- src/components/app/app.ts
+++ src/components/app/app.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import * as AppTpl from './app.html';
+import * as AppSyl from './app.scss';
 
 @Component({
     selector: 'app-root',
-    template: AppTpl
+    template: AppTpl,
+    styles: [AppSyl],
 })
 export class AppComponent {
 }
--- src/components/login/login.ts
+++ src/components/login/login.ts
@@ -1,24 +1,27 @@
 import * as LoginTpl from './login.html';
+import * as LoginSyl from './login.scss';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Component } from "@angular/core";
 import * as firebase from 'firebase/app';
+import { NgForm } from '@angular/forms';
 
 // partof: #SPC-login
 @Component({
     selector: 'login',
     template: LoginTpl,
+    styles: [LoginSyl],
 })
 export class LoginComponent {
-    form = { email: '', password: '' }
+    fields = { email: '', password: '' }
 
     constructor(private angularFireAuth: AngularFireAuth) { }
 
     emailLogin() {
-        this.angularFireAuth.auth.signInWithEmailAndPassword(this.form.email, this.form.password)
+        this.angularFireAuth.auth.signInWithEmailAndPassword(this.fields.email, this.fields.password)
             .then((res) => {
                 console.log(res);
-                this.form.email = '';
-                this.form.password = '';
+                this.fields.email = '';
+                this.fields.password = '';
             });
     }
 }
