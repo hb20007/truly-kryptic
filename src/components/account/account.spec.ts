bfd24d2706dac01a24deeb4c985a25fc9bc38d44
mrbzs <mrbzs123@hotmail.com>
2017-09-17 18:21:42 +0300

    [SPC-account] initial account commit

---
+++ src/components/account/account.spec.ts
@@ -0,0 +1,11 @@
+import '../../required-libs'
+import { AccountComponent } from "./account";
+
+describe('Account Component', () => {
+    it('exists', () => {
+        expect(AccountComponent).toBeDefined();
+
+        // todo - actual tests
+    })
+});
+
