6016ad3587a2165288409bfab18a2460079f5a62
Andreas Stocker <shadowstep7@gmail.com>
2017-09-17 17:57:29 +0300

    [SPC-level-list] add empty component, setup routing

---
+++ src/components/level-list/level-list.spec.ts
@@ -0,0 +1,11 @@
+import '../../test-utils';
+import { LevelListComponent } from './level-list';
+
+describe('Landing-page Component', () => {
+    it('exists', () => {
+        expect(LevelListComponent).toBeDefined();
+
+        // todo - actual tests
+    })
+});
+
