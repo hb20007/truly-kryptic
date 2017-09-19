2d863581164ac540dbd53d06c35bc3226e1437a5
mrbzs <mrbzs123@hotmail.com>
2017-09-19 19:34:12 +0300

    [SPC-account] functional account page without styling

--- src/components/account/account.ts
+++ src/components/account/account.ts
@@ -12,13 +12,67 @@ import {Observable} from "rxjs/Observable";
 })
 
 export class AccountComponent {
+    fields = { currentPassword: '', password: '', confirmPassword: '' };
+    submissionError = '';
 
     username: string;
-    isEmail: Observable<boolean>;
+    hasEmail: boolean;
+    email: string;
 
     constructor(private angularFireAuth: AngularFireAuth) {
         this.username = angularFireAuth.auth.currentUser.displayName;
-        this.isEmail = angularFireAuth.authState.map(x => x.providerData[0].providerId == 'password');
+        this.hasEmail = angularFireAuth.auth.currentUser.providerData[0].providerId == 'password';
+        if (this.hasEmail)
+            this.email = angularFireAuth.auth.currentUser.email;
     }
 
+    deleteUser() {
+        this.angularFireAuth.auth.currentUser.delete();
+    }
+
+    logUserIn() {
+        if (this.fields.currentPassword == '' || this.fields.confirmPassword == '' || this.fields.password == '')
+            this.submissionError = 'Please fill in the required fields';
+        else {
+            this.angularFireAuth.auth.signInWithEmailAndPassword(this.email, this.fields.currentPassword)
+                .then(() => {
+                    this.fields.currentPassword = '';
+                    this.changePass();
+                }).catch((err: any) => {
+                switch (err.code) {
+                    case 'auth/wrong-password':
+                        this.submissionError = 'Wrong Password';
+                        break;
+                    default:
+                        this.submissionError = 'Unknown Error';
+                }
+                throw err;
+            });
+        }
+    }
+
+    changePass() {
+        if (this.fields.password == this.fields.confirmPassword) {
+            this.angularFireAuth.auth.currentUser.updatePassword(this.fields.password)
+                .then(() => {
+                    this.fields.password = '';
+                    this.fields.confirmPassword = '';
+                    this.onAuthSuccess();
+                }).catch((err: any) => {
+                switch (err.code) {
+                    case 'auth/weak-password':
+                        this.submissionError = 'Weak Password';
+                        break;
+                    default:
+                        this.submissionError = 'Unknown Error';
+                }
+                throw err;
+            });
+        }
+        else this.submissionError = 'Passwords do not match';
+    }
+
+    onAuthSuccess() {
+        this.submissionError = 'Password changed successfully';
+    }
 }
