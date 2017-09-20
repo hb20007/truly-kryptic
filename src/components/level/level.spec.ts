c16b6d65496e9cea6a32f0f3adb0dc41d85d0bf3
Andreas Stocker <shadowstep7@gmail.com>
2017-09-20 17:15:56 +0300

    spc-level: add component, setup routing

--- src/components/level/level.spec.ts
+++ src/components/level/level.spec.ts
@@ -1,13 +1,18 @@
 import '../../test-utils';
 import { TestBed } from '@angular/core/testing';
-import { LevelListComponent } from './level-list';
+import { LevelComponent } from './level';
 import { AngularFireAuth } from 'angularfire2/auth';
 
 describe('Landing-page Component', () => {
     beforeEach(() => {
         // TestBed.configureTestingModule({
-        //     declarations: [LevelListComponent],
+        //     declarations: [LevelComponent],
         // }).compileComponents();
+    });
+
+    it('works', () => {
+        // let comp = TestBed.createComponent(LevelComponent).componentInstance;
+        // expect(comp).toBeDefined();
     });
 });
 
