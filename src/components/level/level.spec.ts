f8f09abc9138b1f935d55c9e0395da127da2d0ef
Andreas Stocker <shadowstep7@gmail.com>
2017-09-28 17:29:34 +0300

    spc-level-hint: in progress, filter hints

--- src/components/level/level.spec.ts
+++ src/components/level/level.spec.ts
@@ -2,12 +2,23 @@ import '../../test-utils';
 import { TestBed } from '@angular/core/testing';
 import { LevelComponent } from './level';
 import { AngularFireAuth } from 'angularfire2/auth';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DomSanitizer } from '@angular/platform-browser';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { RouterLinkStubDirective } from '../../test-utils';
 
 describe('Landing-page Component', () => {
     beforeEach(() => {
-        // TestBed.configureTestingModule({
-        //     declarations: [LevelComponent],
-        // }).compileComponents();
+        TestBed.configureTestingModule({
+            declarations: [LevelComponent, RouterLinkStubDirective],
+            providers: [
+                { provide: ActivatedRoute, useValue: {} },
+                { provide: Router, useValue: {} },
+                { provide: DomSanitizer, useValue: {} },
+                { provide: AngularFireDatabase, useValue: {} },
+                { provide: AngularFireAuth, useValue: {} },
+            ],
+        }).compileComponents();
     });
 
     it('works', () => {
