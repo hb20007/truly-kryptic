877086d37e7953b3f48844b1ba387fe614e0a8f3
Andreas Stocker <shadowstep7@gmail.com>
2017-10-09 02:03:04 +0300

    run e2e tests on travis

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,11 +1,19 @@
 require('./src/e2e/patch-wait-for-angular');
 
 exports.config = {
-  allScriptsTimeout: 1500,
-  seleniumAddress: 'http://localhost:4444/wd/hub',
+  allScriptsTimeout: 3500,
+  seleniumAddress: 'http://localhost:4444',
   specs: ['src/e2e/**/*.e2e.js'],
 
   baseUrl: 'http://localhost:8080/',
 
   framework: 'jasmine',
+
+  capabilities: {
+    browserName: 'chrome',
+  
+    chromeOptions: {
+       args: [ "--headless", "--disable-gpu", "--window-size=800,600" ]
+     }
+  }
 };
