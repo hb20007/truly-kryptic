6522579bf72c5411b83ea1a1d86653ab87a5da09
Andreas Stocker <shadowstep7@gmail.com>
2017-10-09 01:25:10 +0300

    patch protractor wait to avoid firebase causing timeout

--- src/e2e/tests.e2e.js
+++ src/e2e/tests.e2e.js
@@ -1,6 +1,1 @@
-describe('App Works', function () {
-    it('is displayed', function () {
-        browser.get('http://localhost:9090/index.html');
-        expect($('h1').isPresent()).toBe(true);
-    });
-});
+require('./navigation');
