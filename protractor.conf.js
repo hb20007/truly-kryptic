c2a9444a2851fc52bf61a85e4ab940d99e4229d6
Andreas Stocker <shadowstep7@gmail.com>
2017-10-09 01:04:52 +0300

    reduce protractor timeout

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,4 +1,9 @@
 exports.config = {
+  allScriptsTimeout: 1500,
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['src/e2e/**/*.e2e.js']
+  specs: ['src/e2e/**/*.e2e.js'],
+
+  baseUrl: 'http://localhost:8080/',
+
+  framework: 'jasmine',
 };
