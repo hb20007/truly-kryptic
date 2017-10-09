7e2be8d67dae2dd7ccfa91c6e52a7896d68d3041
Andreas Stocker <shadowstep7@gmail.com>
2017-10-09 15:12:09 +0300

    SPC-login-reset_password: implement and test

--- src/components/reset-password/reset-password.ts
+++ src/components/reset-password/reset-password.ts
@@ -4,6 +4,7 @@ import { Component } from "@angular/core";
 import { Observable } from 'rxjs/Observable';
 import * as firebase from "firebase/app";
 import { AngularFireAuth } from 'angularfire2/auth';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'reset-password',
@@ -14,9 +15,18 @@ import { AngularFireAuth } from 'angularfire2/auth';
 // partof: #SPC-login-reset_password
 export class ResetPasswordComponent {
 
-    // user: Observable<firebase.User>;
+    fields = { email: '' };
+    submissionError = '';
 
-    // constructor(private angularFireAuth: AngularFireAuth) {
-    //     this.user = angularFireAuth.authState;
-    // }
+    constructor(private router: Router, private angularFireAuth: AngularFireAuth) { }
+
+    sendResetLink() {
+        if (!this.fields.email) {
+            this.submissionError = 'Please enter an email address';
+        } else {
+            this.angularFireAuth.auth.sendPasswordResetEmail(this.fields.email).then(() => {
+                this.submissionError = 'Password reset link sent';
+            });
+        }
+    }
 }
