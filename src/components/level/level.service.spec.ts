25da2212031ea0affbe9c5006bc2fcf2ac753990
Andreas Stocker <shadowstep7@gmail.com>
2017-10-11 11:50:41 +0300

    SPC-level: add firebase to level service

--- src/components/level/level.service.spec.ts
+++ src/components/level/level.service.spec.ts
@@ -1,6 +1,9 @@
 import '../../test-utils';
 import { TestBed } from '@angular/core/testing';
 import { LevelService } from './level.service';
+import { levelsBasic } from './level.mocks';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
 // import { LevelComponent } from './level';
 // import { AngularFireAuth } from 'angularfire2/auth';
 // import { ActivatedRoute, Router } from '@angular/router';
@@ -9,26 +12,37 @@ import { LevelService } from './level.service';
 // import { RouterLinkStubDirective } from '../../test-utils';
 
 describe('Landing-page Component', () => {
-    beforeEach(() => {
 
+    beforeEach(() => {
         TestBed.configureTestingModule({
-            // declarations: [LevelComponent, RouterLinkStubDirective],
             providers: [
                 LevelService,
-                // { provide: ActivatedRoute, useValue: {} },
-                // { provide: Router, useValue: {} },
-                // { provide: DomSanitizer, useValue: {} },
-                // { provide: AngularFireDatabase, useValue: {} },
-                // { provide: AngularFireAuth, useValue: {} },
+                { provide: AngularFireDatabase, useValue: {} },
+                { provide: AngularFireAuth, useValue: {} },
             ],
         }).compileComponents();
     });
 
     it('gets the correct prev and next sublevel', () => {
         let levelService: LevelService = TestBed.get(LevelService);
-        console.log(levelService.prevSublevel())
-        // let comp = TestBed.createComponent(LevelComponent).componentInstance;
-        // expect(comp).toBeDefined();
+
+        expect(levelService.prevSublevelInd(levelsBasic(), { levelIndex: 0, sublevelIndex: 0 }))
+            .toEqual(undefined);
+
+        expect(levelService.prevSublevelInd(levelsBasic(), { levelIndex: 0, sublevelIndex: 1 }))
+            .toEqual({ levelIndex: 0, sublevelIndex: 0 });
+
+        expect(levelService.prevSublevelInd(levelsBasic(), { levelIndex: 1, sublevelIndex: 1 }))
+            .toEqual({ levelIndex: 1, sublevelIndex: 0 });
+
+        expect(levelService.nextSublevelInd(levelsBasic(), { levelIndex: 0, sublevelIndex: 0 }))
+            .toEqual({ levelIndex: 1, sublevelIndex: 0 });
+
+        expect(levelService.nextSublevelInd(levelsBasic(), { levelIndex: 1, sublevelIndex: 0 }))
+            .toEqual({ levelIndex: 1, sublevelIndex: 1 });
+
+        expect(levelService.nextSublevelInd(levelsBasic(), { levelIndex: 1, sublevelIndex: 1 }))
+            .toEqual(undefined);
     });
 });
 
